8732cd7313823942fbfc5e535673fd34ff3472d7
jerinbabujb <jerinbabujb007@gmail.com>
2026-05-04 17:08:55 +0300

    chnaged thefrontend url chnage

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
+
+const API_BASE_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api/v1` 
+  : '/api/v1';
+  
 const api = axios.create({
-  baseURL: '/api/v1',
+  baseURL: 'API_BASE_URL',
   timeout: 30000,
 })
 
